9859a83bde5c7ddb60563ee47b5ae8c0bfb3db39
Nickita Kodatsky <64271364+Nickitas@users.noreply.github.com>
2023-01-23 00:54:17 +0300

    Initial commit

--- Client/src/components/UI/inputs/Input.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import classes from './Input.module.scss'
-
-const Input = ({...props}) => {
-    return (
-        <input {...props} className={classes.Input}/>
-    )
-}
-export default Input;
